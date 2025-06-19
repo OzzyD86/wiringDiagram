6a61f4ccb862c87054d75fcd675960a6a8ee74b0
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2025-06-19 20:52:13 +0100

    Maths plural
    A few large hoops jumped through, but PNG export now supports waypointing. 
    
    Next we'll be turning it off(!) I'm not kidding! That or the interface... maybe the interface

--- incs/diagram.py
+++ incs/diagram.py
@@ -196,7 +196,7 @@ class diagram():
 		return outmap
 		
 	def exportPng(self):
-	
+		olines = {}
 		print(self.bounds)
 		w = int(self.bounds[2] - self.bounds[0])
 		h = int(self.bounds[3] - self.bounds[1])
@@ -208,7 +208,67 @@ class diagram():
 				aa = self.objMk(dr, self.getDevice(i), self.locs[i], offset = (-self.bounds[0], -self.bounds[1]))
 			self.getDevice(i).drwConnPos = aa
 	
-		for i in self.conns.values():
+		d = self
+		for k,i in self.conns.items():
+			p =0
+			pin = None
+			pout = None
+			if (self.getDevice(i[0][0]) is not None):
+				pin = self.getDevice(i[0][0]).connectors[i[0][1]]["direction"]
+			else:
+				p+=1
+			
+			if (self.getDevice(i[1][0]) is not None):
+				pout = self.getDevice(i[1][0]).connectors[i[1][1]]["direction"]
+			else:
+				p+=1
+			
+			if (pin == pout):
+				if (pin is not None):
+					print("Plugged " + str(pin) + " into " + str(pout) + " with", i)
+		
+			if (p == 0):
+				n = []
+				cs = []
+				st = self.getDevice(i[0][0]).drwConnPos[i[0][1]]
+				fn =  self.getDevice(i[1][0]).drwConnPos[i[1][1]]
+				if (k in self.cwps):
+					for l in self.cwps[k]:
+						#print(l)
+						if (l["wpid"] in self.wp):
+							n += self.wp[l["wpid"]]["loc"]
+							cs.append(l["wpid"])
+							#print(d.wp[l["wpid"]]["loc"])
+				#print(n)
+				
+				if (len(cs) > 1):
+					#print(cs)
+					dr.line((st[0]-self.bounds[0], st[1]-self.bounds[1],n[0]-self.bounds[0],n[1]-self.bounds[1]), fill=(0,0,0))
+					dr.line((n[-2]-self.bounds[0], n[-1] ,fn[0]-self.bounds[0], fn[1]-self.bounds[1]), fill=(0,0,0))
+	
+					for m in range(len(cs)-1):
+						q = (cs[m], cs[m+1])
+						#print(q)
+						if ((cs[m], cs[m+1]) in olines):
+							olines[cs[m], cs[m+1]] += 1
+						else:
+							olines[cs[m], cs[m+1]] = 1
+				else:
+					print(st,fn)
+					print("n:",n)
+					if (len(n) == 0):
+						dr.line((st[0]-self.bounds[0], st[1]-self.bounds[1],fn[0]-self.bounds[0], fn[1]-self.bounds[1]), fill=(0,0,0))
+					else:
+						dr.line((st[0]-self.bounds[0], st[1]-self.bounds[1],n[0]-self.bounds[0],n[1]-self.bounds[1]), fill="black")
+						dr.line((n[0]-self.bounds[0], n[1]-self.bounds[1],fn[0]-self.bounds[0], fn[1]-self.bounds[1]), fill="black")
+						
+		for m,n in olines.items():
+			dr.line((self.wp[m[0]]["loc"][0]-self.bounds[0],
+				self.wp[m[0]]["loc"][1]-self.bounds[1], 
+				self.wp[m[1]]["loc"][0]-self.bounds[0],
+				self.wp[m[1]]["loc"][1]-self.bounds[1]), width=n, fill=(0,0,0))
+			print(m,n)
+		'''for i in self.conns.values():
 			pin = self.getDevice(i[0][0]).connectors[i[0][1]]["direction"]
 			pout = self.getDevice(i[1][0]).connectors[i[1][1]]["direction"]
 
@@ -218,5 +278,5 @@ class diagram():
 			
 			st = self.getDevice(i[0][0]).drwConnPos[i[0][1]]
 			fn =  self.getDevice(i[1][0]).drwConnPos[i[1][1]]
-			dr.line((st[0] - self.bounds[0], st[1] - self.bounds[1] ,fn[0] - self.bounds[0], fn[1] - self.bounds[1]), fill=(0,0,0))
+			dr.line((st[0] - self.bounds[0], st[1] - self.bounds[1] ,fn[0] - self.bounds[0], fn[1] - self.bounds[1]), fill=(0,0,0))'''
 		return im
