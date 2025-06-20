23156e00e85cfddb6af85b0651481d4d293dc3a0
OzzyD86 <oliver.dixon.1986@googlemail.com>
2025-06-20 11:15:05 +0100

    Connector widget
    * Added a connector widget
    * Added connector widget to delete wire
    
    The treeview is just for show at the moment, as updates in an as I thought unrelated branch could be effective in what I want to achieve!

---
+++ widgets/connector_points.py
@@ -0,0 +1,67 @@
+import tkinter as Tk
+import tkinter.ttk as ttk
+
+class connector_points(Tk.Frame):
+
+	def setM(self, *what, **kwargs): # This is used a lot, can it be stored elsewhere and imported(?)
+		kwargs['o']["state"]='readonly'
+		
+		KEYS, VALUES = self.machs
+		obj = KEYS[VALUES.index(kwargs['i'])]
+		ii = self.diagram.getDevice(obj).connectors.keys()
+		
+		kwargs['o']["values"]=list(ii)
+		
+	def pass_machines(self, machs):
+		self.machs = machs
+		#print(machs)
+		return self
+		
+	def connector_selector(self, *args, **kwargs):
+	
+		KEYS, VALUES = self.machs
+		obj = KEYS[VALUES.index(self.mName.get())]
+		
+		for i in self.c.get_children():
+			self.c.delete(i)
+		for i in self.diagram.conns:
+			if (i[0][0] == obj) or (i[1][0] == obj):
+				if (i[0][1] == self.cName.get()) or (i[1][1] == self.cName.get()):
+					if (i[0][1] == self.cName.get()):
+						self.c.insert("", "end", values=(i[1][0], i[1][1]))
+					else:
+						self.c.insert("", "end", values=(i[0][0], i[0][1]))
+					
+					#print(i)
+		pass
+		
+	def __init__(self, master, diagram, **kwargs):
+		super().__init__(master, **kwargs)
+		self.cName = Tk.StringVar(self)
+		self.mName = Tk.StringVar(self)
+		self.diagram = diagram
+
+	def go(self):
+		VALUES = self.machs[1]
+
+		Tk.Label(self, text="Machine Name").grid()
+		a = ttk.Combobox(self, state='readonly', textvariable= self.mName, values=VALUES).grid()
+
+		Tk.Label(self, text="Connection Point").grid()
+		self.b = ttk.Combobox(self, state='disabled', textvariable= self.cName, values=VALUES)
+		self.b.grid()
+		self.mName.trace('w', lambda *a, b = self.b: self.setM(i = self.mName.get(), o = b))
+		self.cName.trace('w', self.connector_selector)
+		
+		Tk.Label(self, text="Connection").grid()
+		self.c = ttk.Treeview(self, columns=("DevIn", "ConnIn"), show="headings")
+		self.c.heading("DevIn", text="Input Device")
+		self.c.heading("ConnIn", text="Input Connector")
+		#tree.pack(fill=tk.BOTH, expand=True)
+		self.c.grid()
+		return self
+		
+	def get(self):
+		return (self.mName, self.cName)
+		
+	pass
