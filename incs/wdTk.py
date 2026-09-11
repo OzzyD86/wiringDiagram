9726d37b1cc9cf87ad6eb230d67fdcf720093d39
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2026-09-11 13:48:25 +0100

    Ditto to prior

--- incs/wdTk.py
+++ incs/wdTk.py
@@ -378,52 +378,6 @@ class wdTk(wdTkCore):
 
 		self.aw.destroy()
 		self.redraw()
-	# == Deleting
-	
-	def waypointDelWin(self):
-		#if (len(self.core.dia.listDevices())== 0):
-		#	tkinter.messagebox.showerror(title="No devices", message="There are no devices to delete.")
-		#	return False
-		VALUES = []
-		self.aw = Tk.Tk()
-		for i,j in self.core.dia.wp.items():
-			VALUES.append(j["name"])
-			#print(i)
-
-		#self.mName = Tk.StringVar(self.aw)
-		self.wpName = Tk.StringVar(self.aw)
-
-		ComboEntryWidget(self.aw, text="Waypoint Name", variable=self.wpName, values=VALUES).grid()
-
-		#Tk.Label(self.aw, text="Waypoint Name").grid()
-		#a = ttk.Combobox(self.aw, state='readonly', textvariable= self.wpName, values=VALUES)
-		#a.grid()
-		
-		var = Tk.IntVar()
-		Tk.Checkbutton(self.aw, text="Delete associated paths", variable=var, 
-			onvalue=1, offvalue=0, state=Tk.DISABLED).grid()
-		#Tk.Label(self.aw, text="Human Name").grid()
-		#Tk.Entry(self.aw, textvariable= self.hName).grid()
-		Tk.Button(self.aw, text="Delete", command=self.waypointDelComplete).grid()
-
-	def waypointDelComplete(self):
-		
-		# Load the objects
-		#KEYS, VALUES = self.getKeys()
-		
-		# Find the object
-		#if not self.dhName.get() in VALUES:
-		#	tkinter.messagebox.showerror(title="No device", message="No.")
-		#	return False
-			
-		obj = self.wpName.get()
-		
-		self.core.deleteWaypoint(obj)
-		self.updateWindowTitle()
-
-		self.redraw()
-		self.aw.destroy()
-		pass
 	
 	## === Routing management
 	
