a25ba4efa55deef3901f4beecb236b8a1bb25898
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2026-09-17 14:14:16 +0100

    More files to check

--- widgets/EntryWidget.py
+++ widgets/EntryWidget.py
@@ -5,30 +5,62 @@ class EntryWidget(Tk.Frame):
 	def __init__(self, master=None, text="", variable=None, command=None, *args, **kwargs):
 		super().__init__(master, *args, **kwargs)
 		
-		Tk.Label(self, text=text).grid()
-		Tk.Entry(self, textvariable= variable).grid()
-		
+		Tk.Label(self, text=text).grid(sticky='nesw')
+		Tk.Entry(self, textvariable= variable).grid(sticky='nesw')
+		self.columnconfigure(0, weight=1)
+	#	if (command is not None)
 	pass
 	
 class ComboEntryWidget(Tk.Frame):
-	def __init__(self, master=None, text="", variable=None, command=None, values = [], state='readonly', *args, **kwargs):
+	def __init__(self, master=None, text="", variable=None, command=None, values = {}, state='readonly', *args, **kwargs):
+		if ("cArgs" in kwargs):
+			cargs = kwargs['cArgs']
+			del kwargs['cArgs']
+		else:
+			cargs = {}
 		super().__init__(master, *args, **kwargs)
-
-		Tk.Label(self, text=text).grid()
-		self.box = ttk.Combobox(self, state=state, textvariable= variable, values=values)
-		self.box.grid()
+		self.inter_values = values
+		if (variable is None):
+			self.choice = Tk.StringVar(self)
+		else:
+			self.choice = variable
+		if (type(values) == dict):
+			ls = list(values.values())
+		else:
+			ls = values
+			
+		Tk.Label(self, text=text).grid(sticky='nesw')
+		self.box = ttk.Combobox(self, state=state, textvariable= self.choice, values= ls)
+		self.box.grid(sticky='nesw')
 		if (command is not None):
-			variable.trace('w', command)
+			args = (0, variable)
+			#print(command)
+			self.choice.trace_add('write', lambda *args, **kwargs: command(*cargs, self.choice.get()))
+		self.columnconfigure(0, weight=1)
 
 	def setState(self, state='readonly'):
 		self.box['state'] = state
 	
-	def setValues(self, values = []):
-		self.box['values'] = values
+	def setValues(self, values = {}):
+		self.inter_values = values
+		if (type(values) == dict):
+			ls = list(values.values())
+		else:
+			ls = values
+		self.box['values'] = ls
+
+	def get(self):
+		return self.choice
+		
+	def get_key_of_value(self, value):
+		for key, val in self.inter_values.items():
+			if str(val) == str(value):
+				return key
+		return None
 		
 class SpinEntryWidget(Tk.Frame):
 	def __init__(self, master = None, text="", variable=None, min=0, max=100, command=None, *args, **kwargs):
 		super().__init__(master, *args, **kwargs)
-		Tk.Label(self, text=text).grid()
-		Tk.Spinbox(self, from_=min, to=max, textvariable=variable).grid()
-		
+		Tk.Label(self, text=text).grid(sticky='nesw')
+		Tk.Spinbox(self, from_=min, to=max, textvariable=variable).grid(sticky='nesw')
+		self.columnconfigure(0, weight=1)
---
+++ widgets/inputDialog.py
@@ -0,0 +1,69 @@
+import tkinter as Tk
+import tkinter.ttk as ttk
+#from incs.wdCore import wdCore
+#from widgets.connector_points import connector_points
+from widgets.EntryWidget import EntryWidget, ComboEntryWidget, SpinEntryWidget
+#import tkinter.messagebox
+
+class inputDialog(Tk.Toplevel):
+	def __init__(self, master, data={}, **kwargs):
+		super().__init__(master, **kwargs)
+		self.data = data # This will be useful later!
+		self.vars = {}
+		self.funcs = {}
+		for i, j in data.items():
+			self.vars[i] = Tk.StringVar(self)
+			#print(j)
+			if ("onUpdate" in j.keys()):
+				c = j['onUpdate']
+			else:
+				c = None
+			if ("updateVars" in j.keys()):
+				cc = j["updateVars"]
+			else:
+				cc = []
+			if ("value" in j):
+				self.vars[i].set(j["value"])
+				
+			if (j["type"] in ["Entry"]):
+				j["obj"] = EntryWidget(self, text=j["name"], variable=self.vars[i])#.grid(padx=5, pady=(5,0), sticky='nsew')
+			elif (j["type"] in ["Combo"]):
+				j["obj"] = ComboEntryWidget(self, text=j["name"], variable=self.vars[i], values=j['values'], command= c, cArgs=(self, cc))#.grid(sticky='nsew')		
+			elif (j["type"] in ["Label", "Text"]):
+				j["obj"] = Tk.Label(self, text= j["text"], wraplength=800) #.grid(padx=5,pady=(5,0), sticky='sewn')
+			elif (j["type"] in ["Spin"]):
+				j["obj"] = SpinEntryWidget(self, text=j["name"], min=1, max=32, variable=self.vars[i])
+			elif(j["type"] in ["Checkbox"]):
+				j["obj"] = Tk.Checkbutton(self, text=j["name"], variable=self.vars[i], 
+					onvalue=1, offvalue=0, state=Tk.DISABLED)
+	
+			j["obj"].grid(padx=5, pady=(5,0), sticky='nsew')
+		self.columnconfigure(0, weight=1)
+
+	def addButton(self, title, local_action = "add"):
+		self.funcs[local_action] = []
+		Tk.Button(self, text=title, command=lambda: self.button_press(local_action)).grid(padx=5, pady=(5, 0), sticky='swen')
+	
+	def set(self, op, val):
+		self.vars[op].set(val)
+
+	def passFunc(self, loc, func):
+		self.funcs[loc].append(func)
+		
+	def button_press(self, mode):
+		snd={}
+		#print("Do stuff here") 
+		for i,j in self.vars.items():
+			if (type(j) is Tk.StringVar):
+				if (self.data[i]["type"] in ["Combo"] and type(self.data[i]["values"]) is dict):
+					snd[i] = self.data[i]["obj"].get_key_of_value(j.get())
+				else:
+					snd[i] = j.get()
+			else:
+				print("Slight panic:", i, j)
+					
+		for i in self.funcs[mode]:
+			t = i(**snd)
+		
+		if (t):
+			self.destroy()
