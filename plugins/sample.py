0bba629017d0e5f7ee1b4f490eb4b106ccbaa52d
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2026-09-23 20:21:49 +0100

    THIS
    Absolutely no comment!

--- plugins/sample.py
+++ plugins/sample.py
@@ -2,6 +2,25 @@ print("HELLO!")
 
 class sample():
 	pass
+
+def devAddWin(s, **kwargs):
+	return { "Name": "Sample",
+		"Widgets" : {
+			"Test": {
+				"type" : "Entry",
+				"name": "Test"
+			}
+		}
+	}
+
+def a(self, w, val, *args):
+	return { "Test" : "Get data here" }
 	
 MANIFEST = {
+	"order": 0,
+	"events": {
+		"onDeviceAddDialog" : [devAddWin],
+		"onDeviceEditDialog" : [devAddWin],
+		#"onMachineNameSet" : [a]
+	}
 }
