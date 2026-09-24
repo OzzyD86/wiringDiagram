8f81943ab0f2c8c49b09435ee47526a9b7e507d7
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2026-09-24 09:51:43 +0100

    QED
    Ooh. Should I just pass wd to the plugin?

--- plugins/sample.py
+++ plugins/sample.py
@@ -1,4 +1,5 @@
 print("HELLO!")
+from tkinter import Menu
 
 class sample():
 	pass
@@ -13,14 +14,30 @@ def devAddWin(s, **kwargs):
 		}
 	}
 
+def devAddComplete(**kwargs):
+	raise Exception(kwargs)
+	
 def a(self, w, val, *args):
 	return { "Test" : "Get data here" }
+
+def menu(self):
+	x = Menu()
+	x.add_command(label= "Hello", state="disabled")
+	
+	return {
+		"name" : "Sample",
+		"menu" : x
+	}
 	
 MANIFEST = {
 	"order": 0,
 	"events": {
+		"onMenuSpawn" : [menu],
 		"onDeviceAddDialog" : [devAddWin],
 		"onDeviceEditDialog" : [devAddWin],
 		#"onMachineNameSet" : [a]
+		
+		"onDeviceAddComplete" : [devAddComplete],
+		#"onDeviceEditComplete" : [devAddWin],
 	}
 }
